65c72ccbd15f5f17b786909f301d8cc5c67bb018
Victor Gustafsson <kerrik.vg@gmail.com>
2024-05-20 15:08:24 +0200

    Employee class.

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,7 +1,28 @@
 ﻿namespace Personalregister {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            Employee e = new Employee("Bob", 25000);
+            Console.WriteLine(e.Name + "-" + e.Salary);
+        }
+    }
+
+    public class Employee {
+        private string _name;
+        private int _salary;
+
+        public Employee(string name, int salary) {
+            _name = name;
+            _salary = salary;
+        }
+
+        public string Name {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Salary {
+            get { return _salary; }
+            set { _salary = value; }
         }
     }
 }
